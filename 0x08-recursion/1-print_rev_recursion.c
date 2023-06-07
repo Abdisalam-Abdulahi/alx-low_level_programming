3c407116c83bb98c7f4d86c704d890964d3e3d1c
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-06-07 12:25:58 +0300

    fixing chevker error

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,11 +6,11 @@
 
 void _print_rev_recursion(char *s)
 {
+	if (*s == '\0')
+		_putchar('\n');
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	if (*s == '\0')
-		_putchar('\n');
 }
