8348d62f47d127385990f5d39a842d1185407bae
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-06-10 21:22:10 +0300

    0x0A-argc_argv/

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - the main function it prints its name, followed by a new line
+  *@argc: counts the arguments passed to the function
+  *@argv: is a one-dimensional array of strings
+  *Return: 0 for success
+  */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
