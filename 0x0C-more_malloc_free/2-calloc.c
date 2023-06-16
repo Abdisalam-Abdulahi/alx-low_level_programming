85892d325b6631ee955dd2e2e986127c801ae31a
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-06-16 09:08:47 +0300

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char **ptr;
 	int i, len;
 
-	if (nmemeb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	len = sizeof(nmemb);
 	ptr = malloc(size * nmemb);
