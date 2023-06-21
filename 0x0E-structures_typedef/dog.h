b3fe8ae6ac98d4638a930985be1c66861d75ba86
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-06-21 15:11:49 +0300

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+  *struct dog - simple staruct
+  *@name: the name of the dog
+  *@age: the age of dog
+  *@owner: the owner
+  *
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
