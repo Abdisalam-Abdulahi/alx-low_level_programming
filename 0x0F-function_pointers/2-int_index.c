7fa60fe4c2cb04af600d4e20728482cf50a6c5ba
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-06-22 17:17:35 +0300

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,7 +5,8 @@
   *@array: the array to be searched in
   *@size: the size of the array
   *@cmp: compares values
-  *
+  *Return: returns the index of the first element for which
+  *the cmp function does not return 0
   */
 
 int int_index(int *array, int size, int (*cmp)(int))
