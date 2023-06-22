057cca0ae4fc4f9f17427f0d7fbaea1d33029baf
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-06-22 22:21:15 +0300

    simple calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main -the main func
+  *@argc: argument counter
+  *@argv: the arguments it self
+  *Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int result, arg1, arg2;
+	char ptr;
+	int (*func)(int, int);
+
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	ptr = *argv[2];
+	if ((ptr == '/' || ptr == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = func(arg1, arg2);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *op_add - adds two integers
+  *@a: first
+  *@b: second
+  *Return: result of the operation
+  */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+  *op_sub - subtracts two integers
+  *@a: first
+  *@b: second
+  *Return: result of the operation
+  */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  *op_mul - multiplies two integers
+  *@a: first
+  *@b: second
+  *Return: result of the operation
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  *op_div - divides two integers
+  *@a: first
+  *@b: second
+  *Return: result of the operation
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  *op_mod - modolize two integers
+  *@a: first
+  *@b: second
+  *Return: result of the operation
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
