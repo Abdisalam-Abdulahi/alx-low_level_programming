7a335dac03cada635fa6f9a2ba2c0f291b818a1e
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-07-06 12:33:01 +0300

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  *print_binary - converts a number in a binary form
+  *@n: the number to be converted
+  */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int shifts;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
