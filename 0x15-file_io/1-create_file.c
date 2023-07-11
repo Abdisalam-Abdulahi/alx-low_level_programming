495b4e386a2a967fe38ecf4234c38087c467624c
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-07-11 17:32:05 +0300

    fixing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		len = 0;
-		while (text_content[len] != '\0')
+		while (text_content[len])
 			len++;
 	}
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
