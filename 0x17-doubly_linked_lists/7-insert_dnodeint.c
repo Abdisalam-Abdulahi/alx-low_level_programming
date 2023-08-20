a00e815ef232c0bb24118fdf9becd26e013430d7
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-08-20 11:37:27 +0300

    fixing edgcases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,50 +8,44 @@
   */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h;
-	dlistint_t *tmp2;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-	unsigned int size = len(*h);
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	if (idx > size)
-		return (NULL);
+	new = NULL;
 	if (idx == 0)
-		add_dnodeint(h, n);
-	else if (idx == size)
-		add_dnodeint_end(h, n);
+		new = add_dnodeint(h, n);
 	else
 	{
-		new->n = n;
-		new->next = NULL;
-		while (--idx)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			tmp = tmp->next;
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
 		}
-		tmp2 = tmp->next;
-		new->next = tmp->next;
-		new->prev = tmp;
-		tmp->next = new;
-		tmp2->prev = new;
 	}
 
 	return (new);
-}
-
-
-/**
-  *len - return the length of the list
-  *@head: points to the first node;
-  *Return: the length of the list
-  */
-int len(dlistint_t *head)
-{
-	dlistint_t *tmp = head;
-	unsigned int n = 0;
-
-	while (tmp)
-	{
-		tmp = tmp->next;
-		n++;
-	}
-	return (n);
 }
