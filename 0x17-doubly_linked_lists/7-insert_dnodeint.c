4d7d53592e60c015149ac0063d0ed96d4587f42c
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-08-20 10:44:18 +0300

    fixing edgcases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,23 +9,49 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h;
+	dlistint_t *tmp2;
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-	unsigned int i = 0;
+	unsigned int size = len(*h);
 
-	if (new == NULL)
+	if (idx > size)
 		return (NULL);
-	while (tmp && i < idx)
+	if (idx == 0)
+		add_dnodeint(h, n);
+	else if (idx == size)
+		add_dnodeint_end(h, n);
+	else
+	{
+		new->n = n;
+		new->next = NULL;
+		while (--idx)
+		{
+			tmp = tmp->next;
+		}
+		tmp2 = tmp->next;
+		new->next = tmp->next;
+		new->prev = tmp;
+		tmp->next = new;
+		tmp2->prev = new;
+	}
+
+	return (new);
+}
+
+
+/**
+  *len - return the length of the list
+  *@head: points to the first node;
+  *Return: the length of the list
+  */
+int len(dlistint_t *head)
+{
+	dlistint_t *tmp = head;
+	unsigned int n = 0;
+
+	while (tmp)
 	{
 		tmp = tmp->next;
-		i++;
+		n++;
 	}
-	new->n = n;
-	new->prev = tmp->prev;
-	tmp->prev = new;
-	new->next = tmp;
-	if (new->prev != NULL)
-		new->prev->next = new;
-	else
-		*h = new;
-	return (new);
+	return (n);
 }
