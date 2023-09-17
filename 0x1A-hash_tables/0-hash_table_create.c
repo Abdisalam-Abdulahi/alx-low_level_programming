82ef6ca9177397951f9223586806d99800fcd44b
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-09-17 14:43:49 +0300

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *hash_table_create - function that creates a hash table
+  *@size: is the size of the array
+  *Return: pointer to the newly created hash table
+  */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_ptr = malloc(sizeof(hash_table_t));
+
+	if (hash_ptr == NULL)
+		return (NULL);
+	hash_ptr->size = size;
+	hash_ptr->array = calloc(hash_ptr->size, sizeof(hash_node_t));
+
+	return (hash_ptr);
+}
+
