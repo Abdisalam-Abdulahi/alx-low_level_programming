de9b3c6103bd0f722689d0c8747ad08e403e9c26
Abdisalam-Abdulahi <iidlex99@gmail.com>
2025-05-17 07:56:22 +0300

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  *_strncpy - appends the src string to the dest string
+  *@src: sources string
+  *@dest: destination string
+  *@n: n bytes to concatenate
+  *Return: char poniter
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+	/*
+		*if (src[i] == '\0')
+		*{
+		*	break;
+		*}
+	 */
+		dest[i] = src[i];
+		i++;
+	}
+/*	dest[i] = '\0';*/
+	return (dest);
+}
