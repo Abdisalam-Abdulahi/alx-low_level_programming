420ea698533efd692723a9954d01341b15cf5b3a
Abdisalam-Abdulahi <iidlex99@gmail.com>
2025-05-21 06:52:14 +0300

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+/**
+  * _strchr - a function that copies memory from src to dest.
+  *@s: char pointer
+  *@c: the chracter to locate
+  *Return: char pointer
+  */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	i++;
+	}
+	return (NULL);
+}
